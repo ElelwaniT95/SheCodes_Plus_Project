3f0108208b10ab252f069eca782f1bf0a3c00f7b
Elelwani <tshikosiem@gmai.com>
2023-08-27 23:54:01 +0200

    index.html

--- src/index.js
+++ src/index.js
@@ -139,5 +139,5 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
-search("Johannesburg");
+search("Polokwane");
 displayForecast();
